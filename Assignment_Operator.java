54491b43e99f8450d08af0a83f26a66160b10575
Swarup kumar sahoo <kumarswarup7272@gmail.com>
2025-01-20 23:13:32 +0530

    basic java

---
+++ Assignment_Operator.java
@@ -0,0 +1,33 @@
+
+public class Assignment_Operator{
+
+    public static void main(String[] args) {
+        
+        int a = 18;
+        int b = 13;
+
+        //Assignment operator
+
+        //Add
+        int sum  = a + b;
+        System.out.println("Sum of a and b is: " + sum);
+
+        //Subtract
+        int sub = a - b;
+        System.out.println("Subtraction of a and b is: " + sub);
+
+        //Multiply
+        int mul = a * b;
+        System.out.println("Multiplying a and b is: " + mul);
+
+        //Divide
+        int div = a / b;
+        System.out.println("Dividing a and b is :" + div);
+
+         //Modulus
+         int mod = a % b;
+         System.out.println("Reminder a and b is :" + mod);
+        
+
+    }
+}
---
+++ RelationalOperator.java
@@ -0,0 +1,16 @@
+public class RelationalOperator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        // Relational operator examples
+        System.out.println("a == b: " + (a == b)); // false
+        System.out.println("a != b: " + (a != b)); // true
+        System.out.println("a > b: " + (a > b));   // false
+        System.out.println("a < b: " + (a < b));   // true
+        System.out.println("a >= b: " + (a >= b)); // false
+        System.out.println("a <= b: " + (a <= b)); // true
+    }
+}
+
+
