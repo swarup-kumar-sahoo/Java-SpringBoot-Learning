3d9c759a0db2f5e2ad93c9c09189f6a12dff4c0a
Swarup kumar sahoo <kumarswarup7272@gmail.com>
2025-02-02 18:37:32 +0530

    core java

--- inheritance/CalcApp.java
+++ inheritance/CalcApp.java
@@ -2,17 +2,19 @@
 public class CalcApp{
     public static void main(String args[]){
 
-        AdvCalc a = new AdvCalc(); //AdvClac have inherit Calc class so we dont need to declare a Calc class.
+        VeryAdvCalc a = new VeryAdvCalc(); //AdvClac have inherit Calc class so we dont need to declare a Calc class.
         //Benifits of inheritance is it reduces Redudancy.
-        int resultAdd = a.add(12,13);
-        int resultSub = a.sub(40, 20);
-        int resultMul = a.mul(10,10);
-        int resultDiv = a.div(20,2);
+        int resultAdd = a.add(4,6);
+        int resultSub = a.sub(4, 6);
+        int resultMul = a.mul(4,6);
+        int resultDiv = a.div(4,6);
+        int resultPow = (int)a.power(4,6);
 
         System.out.println("Addition: " + resultAdd);
         System.out.println("Subtraction: " + resultSub);
         System.out.println("Multiplication: " + resultMul);
         System.out.println("Division: " + resultDiv);
+        System.err.println("Power: " + resultPow);
 
 
     }
---
+++ inheritance/VeryAdvCalc.java
@@ -0,0 +1,8 @@
+// Multilevel Calculator
+class VeryAdvCalc extends AdvCalc{
+    public double power(double n1,double n2){
+        return Math.pow(n1, n2);
+    }
+}
+
+
